3e54db6ba680cfcd8068215c2aeffc0585692d79
Global-Hack <global_hack_com_2017@yahoo.co.jp>
2023-01-09 10:53:37 +0900

    flet to do

--- flet_test_app/main.py
+++ flet_test_app/main.py
@@ -1,7 +1,50 @@
-import flet
-from flet import Page, Text
+import flet as ft
 
-def main(page: Page):
-	page.add(Text(value="Hello World !!!!"))
+class TodoApp(ft.UserControl):
+    def build(self):
+        self.new_task = ft.TextField(hint_text="Whats needs to be done?", expand=True)
+        self.tasks = ft.Column()
 
-flet.app(target=main)
+        # application's root control (i.e. "view") containing all other controls
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+                self.tasks,
+            ],
+        )
+
+    def add_clicked(self, e):
+        self.tasks.controls.append(ft.Checkbox(label=self.new_task.value))
+        self.new_task.value = ""
+        self.update()
+
+
+def main(page: ft.Page):
+    page.title = "ToDo App"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
+
+    # create application instance
+    todo = TodoApp()
+
+    # add application's root control to the page
+    page.add(todo)
+
+ft.app(target=main)
+
+# import flet,webview
+# from flet import Page, Text
+
+# def main(page: Page):
+# 	contents = webview.create_window('Hello world', 'https://pywebview.flowrl.com/')
+# 	print(contents)
+# 	# page.controls.append(contents)
+# 	page.add(Text(value="Hello World !!!!"))
+
+# flet.app(target=main)
